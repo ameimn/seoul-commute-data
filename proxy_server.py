b718d810f433dfd13a1a0e3edeaf142cf6b8d0d6
ameimn <kimjk8374@gmail.com>
2025-06-04 00:35:45 +0900

    Update proxy_server.py

--- proxy_server.py
+++ proxy_server.py
@@ -46,4 +46,6 @@ def get_commute():
     })
 
 if __name__ == '__main__':
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render에서 포트 자동 할당
+    app.run(host="0.0.0.0", port=port)
